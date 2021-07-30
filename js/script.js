6bb26410351df357d48445fb00006fdfdf339183
Natalya <volchenkova93@gmail.com>
2021-07-30 14:29:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use sctrict";
+
+//alert('hello');
+
+
+// const result = confirm ("you?");
+
+// console.log(result);
+
+// const answer = prompt("э, чо?", "норм");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt ('Who name?', '');
+answers[1] = prompt ('Who firstname?', '');
+answers[2] = prompt ('age??', '');
+
+document.write(answers);
